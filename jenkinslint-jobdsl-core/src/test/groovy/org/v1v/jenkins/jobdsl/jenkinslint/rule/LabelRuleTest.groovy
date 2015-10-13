14ff51b0f0123bc38e7474bf0ebc2635bc943518
Victor Martinez <VictorMartinezRubio@gmail.com>
2015-10-13 13:55:45 +0200

    Refactor

--- jenkinslint-jobdsl-core/src/test/groovy/org/v1v/jenkins/jobdsl/jenkinslint/rule/LabelRuleTest.groovy
+++ jenkinslint-jobdsl-core/src/test/groovy/org/v1v/jenkins/jobdsl/jenkinslint/rule/LabelRuleTest.groovy
@@ -1,7 +1,15 @@
 package org.v1v.jenkins.jobdsl.jenkinslint.rule
 
+import javaposse.jobdsl.dsl.FileJobManagement
 import javaposse.jobdsl.dsl.Item
+import javaposse.jobdsl.dsl.jobs.BuildFlowJob
 import javaposse.jobdsl.dsl.jobs.FreeStyleJob
+import javaposse.jobdsl.dsl.jobs.IvyJob
+import javaposse.jobdsl.dsl.jobs.MatrixJob
+import javaposse.jobdsl.dsl.jobs.MavenJob
+import javaposse.jobdsl.dsl.jobs.MultiJob
+import javaposse.jobdsl.dsl.jobs.WorkflowJob
+import org.junit.Before
 import org.junit.Test
 
 /**
@@ -11,18 +19,102 @@ import org.junit.Test
  */
 class LabelRuleTest {
 
+    private rule
+
+    @Before
+    void setUp() {
+        rule = new LabelRule()
+    }
+
     @Test
-    void testFreeStyleJobWithoutLabel() {
+    void testFreeStyleJobWithoutDescription() {
         Item job = new FreeStyleJob()
-        LabelRule rule = new LabelRule()
         assert rule.isDefect(job)
     }
 
     @Test
-    void testFreeStyleJobWithLabel() {
+    void testMavenJobWithoutDescription() {
+        Item job = new MavenJob(new FileJobManagement(new File(".")))
+        assert rule.isDefect(job)
+    }
+
+    @Test
+    void testMatrixJobWithoutDescription() {
+        Item job = new MatrixJob()
+        assert rule.isDefect(job)
+    }
+
+    @Test
+    void testMultiJobWithoutDescription() {
+        Item job = new MultiJob(new FileJobManagement(new File(".")))
+        assert rule.isDefect(job)
+    }
+
+    @Test
+    void testWorkflowJobWithoutDescription() {
+        Item job = new WorkflowJob()
+        assert rule.isDefect(job)
+    }
+
+    @Test
+    void testBuildFlowJobWithoutDescription() {
+        Item job = new BuildFlowJob()
+        assert rule.isDefect(job)
+    }
+
+    @Test
+    void testIvyJobWithoutDescription() {
+        Item job = new IvyJob()
+        assert rule.isDefect(job)
+    }
+
+    @Test
+    void testFreeStyleJobWithDescription() {
         Item job = new FreeStyleJob()
         job.label('something')
-        LabelRule rule = new LabelRule()
         assert !rule.isDefect(job)
     }
+
+    @Test
+    void testMavenJobWithDescription() {
+        Item job = new MavenJob(new FileJobManagement(new File(".")))
+        job.label('something')
+        assert !rule.isDefect(job)
+    }
+
+    @Test
+    void testMatrixJobWithDescription() {
+        Item job = new MatrixJob()
+        job.label('something')
+        assert !rule.isDefect(job)
+    }
+
+    @Test
+    void testMultiJobWithDescription() {
+        Item job = new MultiJob(new FileJobManagement(new File(".")))
+        job.label('something')
+        assert !rule.isDefect(job)
+    }
+
+    @Test
+    void testWorkflowJobWithDescription() {
+        Item job = new WorkflowJob()
+        job.label('something')
+        assert !rule.isDefect(job)
+    }
+
+    @Test
+    void testBuildFlowJobWithDescription() {
+        Item job = new BuildFlowJob()
+        job.label('something')
+        assert !rule.isDefect(job)
+    }
+
+    @Test
+    void testIvyJobWithDescription() {
+        Item job = new IvyJob()
+        job.label('something')
+        assert !rule.isDefect(job)
+    }
+
 }
