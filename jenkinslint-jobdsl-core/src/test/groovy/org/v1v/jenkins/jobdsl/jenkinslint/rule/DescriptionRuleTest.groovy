d7f4fe9b422878b5677c2d822729e4c54f9d7456
Victor Martinez <VictorMartinezRubio@gmail.com>
2015-10-13 13:44:33 +0200

    Added MavenJob Type in the test case of Description Rule

--- jenkinslint-jobdsl-core/src/test/groovy/org/v1v/jenkins/jobdsl/jenkinslint/rule/DescriptionRuleTest.groovy
+++ jenkinslint-jobdsl-core/src/test/groovy/org/v1v/jenkins/jobdsl/jenkinslint/rule/DescriptionRuleTest.groovy
@@ -1,7 +1,10 @@
 package org.v1v.jenkins.jobdsl.jenkinslint.rule
 
+import javaposse.jobdsl.dsl.FileJobManagement
 import javaposse.jobdsl.dsl.Item
+import javaposse.jobdsl.dsl.JobManagement
 import javaposse.jobdsl.dsl.jobs.FreeStyleJob
+import javaposse.jobdsl.dsl.jobs.MavenJob
 import org.junit.Test
 
 /**
@@ -19,8 +22,23 @@ class DescriptionRuleTest {
     }
 
     @Test
+    void testMavenJobWithoutDescription() {
+        Item job = new MavenJob(new FileJobManagement(new File(".")))
+        DescriptionRule rule = new DescriptionRule()
+        assert rule.isDefect(job)
+    }
+
+    @Test
     void testFreeStyleJobWithDescription() {
         Item job = new FreeStyleJob()
+        job.description('something')
+        DescriptionRule rule = new DescriptionRule()
+        assert !rule.isDefect(job)
+    }
+
+    @Test
+    void testMavenJobWithDescription() {
+        Item job = new MavenJob(new FileJobManagement(new File(".")))
         job.description('something')
         DescriptionRule rule = new DescriptionRule()
         assert !rule.isDefect(job)
